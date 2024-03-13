f7c758480972d8eef5efa94615b8f0784e06eeb1
Yuki Sakai <yuki.sakai.002@layerx.co.jp>
2024-03-13 11:51:23 +0900

    semantic color, primitive colorに対応

--- src/formats.js
+++ src/formats.js
@@ -4,72 +4,91 @@ const _template = require('lodash/template');
 const _ = require('style-dictionary/lib/utils/es6_');
 const { fileHeader, sortByReference, sortByName, } = require('style-dictionary/lib/common/formatHelpers');
 
-const supportedCategories = ['color', 'custom-gradient', 'custom-fontStyle', 'dimension', 'custom-spacing', 'radius', 'breakpoints', 'custom-icon', 'spacing', 'typography'];
+const supportedCategories = [
+  "color",
+  "custom-gradient",
+  "custom-fontStyle",
+  "dimension",
+  "custom-spacing",
+  "radius",
+  "breakpoints",
+  "custom-icon",
+  "spacing",
+  "typography",
+  "semantic color",
+  "primitive color",
+];
 
 function groupBy(list, keyGetter) {
-    var map = {};
-    list.forEach((item) => {
-        const key = keyGetter(item);
-        const collection = map[key];
-        if (!collection) {
-            map[key] = [item];
-        } else {
-            collection.push(item);
-        }
-    });
-    return map;
+  var map = {};
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map[key];
+    if (!collection) {
+      map[key] = [item];
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
 }
 
 function groupTokensByCategory(dictionary, options) {
-    var allTokens;
+  var allTokens;
 
-    const { outputReferences } = options;
+  const { outputReferences } = options;
 
-    if (outputReferences) {
-        allTokens = [...dictionary.allTokens].sort(sortByReference(dictionary));
-    } else {
-        allTokens = [...dictionary.allTokens].sort(sortByName);
+  if (outputReferences) {
+    allTokens = [...dictionary.allTokens].sort(sortByReference(dictionary));
+  } else {
+    allTokens = [...dictionary.allTokens].sort(sortByName);
+  }
+
+  allTokens = allTokens.filter(token => supportedCategories.indexOf(token.type) >= 0)
+
+  return groupBy(allTokens, function (token) {
+
+    switch (token.attributes.category) {
+      case "breakpoints":
+        return "Breakpoint";
+      case "sizes":
+        return "Size";
+      case "semantic color":
+        return "SemanticColor";
+      case "primitive color":
+        return "PrimitiveColor";
+      case "frame size":
+        return "FrameSize";
+      default:
+        return _.upperFirst(token.attributes.category);
     }
-
-    allTokens = allTokens.filter(token => supportedCategories.indexOf(token.type) >= 0)
-
-    return groupBy(allTokens, function (token) {
-
-        switch (token.attributes.category) {
-            case 'breakpoints':
-                return 'Breakpoint';
-            case 'sizes':
-                return 'Size';
-            default:
-                return _.upperFirst(token.attributes.category);
-        }
-    });
+  });
 }
 
 module.exports = {
-    'flutter/theme/theme.dart': function ({ dictionary, options, file }) {
-        const template = _template(
-            fs.readFileSync(__dirname + '/templates/theme/theme.dart.template')
-        );
+  'flutter/theme/theme.dart': function ({ dictionary, options, file }) {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/theme/theme.dart.template')
+    );
 
-        return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, file, options, fileHeader });
-    },
-    'flutter/theme/data.dart': function ({ dictionary, options, file }) {
-        const template = _template(
-            fs.readFileSync(__dirname + '/templates/theme/data.dart.template')
-        );
-        return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, toDartType, file, options, fileHeader });
-    },
-    'flutter/theme/widgets.dart': function ({ dictionary, options, file }) {
-        const template = _template(
-            fs.readFileSync(__dirname + '/templates/theme/widgets.dart.template')
-        );
-        return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, file, options, fileHeader });
-    },
-    'flutter/theme/gallery.dart': function ({ dictionary, options, file }) {
-        const template = _template(
-            fs.readFileSync(__dirname + '/templates/theme/gallery.dart.template')
-        );
-        return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, pascalCase: (str) => _.upperFirst(_.camelCase(str)), toDartType, file, options, fileHeader });
-    },
+    return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, file, options, fileHeader });
+  },
+  'flutter/theme/data.dart': function ({ dictionary, options, file }) {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/theme/data.dart.template')
+    );
+    return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, toDartType, file, options, fileHeader });
+  },
+  'flutter/theme/widgets.dart': function ({ dictionary, options, file }) {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/theme/widgets.dart.template')
+    );
+    return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, file, options, fileHeader });
+  },
+  'flutter/theme/gallery.dart': function ({ dictionary, options, file }) {
+    const template = _template(
+      fs.readFileSync(__dirname + '/templates/theme/gallery.dart.template')
+    );
+    return template({ allTokens: groupTokensByCategory(dictionary, options), camelCase: _.camelCase, pascalCase: (str) => _.upperFirst(_.camelCase(str)), toDartType, file, options, fileHeader });
+  },
 }
